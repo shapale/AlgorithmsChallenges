43a9345e6fa1c38feedbe3e5cf17d516d0ee1169
shapale <71771058+shapale@users.noreply.github.com>
2020-12-30 15:13:43 +0800

    Edited Solution_2
    edited the solution_2 code

--- EasyChallenges/Challenge1/Solution_2.py
+++ EasyChallenges/Challenge1/Solution_2.py
@@ -1,8 +1,10 @@
-def twoNumberSum(array, targetNum):
-    for i in range(len(array) - 1):
-        firstNum = array[i]
-        for j in range(i+1,len(array)):
-            secondNum = array[j]
-            if firstNum + secondNum == targetNum:
-                return [firstNum,secondNum]
+def twoNumberSum(array, targetSum):
+    nums = {}
+    for num in array:
+        potentialMatch = targetSum - num
+        if potentialMatch in nums:
+            return [targetSum - num, num]
+        else:
+
+            nums[num] = True
     return []
